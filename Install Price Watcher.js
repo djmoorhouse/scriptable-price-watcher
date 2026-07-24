b0bec6c61b5e1507da5076e8e941b309866d7da2
djmoorhouse <admin@thetaxreturnexpert.co.uk>
2026-07-24 10:42:59 +0300

    Install analytics module with Price Watcher

--- Install Price Watcher.js
+++ Install Price Watcher.js
@@ -1,6 +1,6 @@
 // One-tap installer for Scriptable Price Watcher
 const BASE = "https://raw.githubusercontent.com/djmoorhouse/scriptable-price-watcher/main/";
-const FILES = ["Price Watcher.js", "PW_Storage.js", "PW_Scraper.js"];
+const FILES = ["Price Watcher.js", "PW_Storage.js", "PW_Scraper.js", "PW_Analytics.js"];
 const fm = FileManager.iCloud();
 const dir = fm.documentsDirectory();
 
@@ -14,7 +14,7 @@ for (const name of FILES) {
 
 const a = new Alert();
 a.title = "Price Watcher installed";
-a.message = "Open the new ‘Price Watcher’ script in Scriptable and run it.";
+a.message = "Price Watcher and its analytics engine are up to date. Open ‘Price Watcher’ in Scriptable and run it.";
 a.addAction("OK");
 await a.presentAlert();
 Script.complete();
